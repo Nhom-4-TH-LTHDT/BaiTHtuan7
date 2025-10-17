328af447fcb516a3832be734ac8df817edb4d752
huynhcongvu <congvuhuynh9@gmail.com>
2025-10-17 17:52:27 +0700

    Update Sach.java

--- VU_btaptuan7/Sach.java
+++ VU_btaptuan7/Sach.java
@@ -1,11 +1,15 @@
 public abstract class Sach {
-    protected String tieuDe;
-    protected String tacGia;
-    protected int namXuatBan;
-    protected int soLuong;
-    protected double giaCoBan;
+    private String maSach;
+    private String tieuDe;
+    private String tacGia;
+    private int namXuatBan;
+    private int soLuong;
+    private double giaCoBan;
 
-    public Sach(String tieuDe, String tacGia, int namXuatBan, int soLuong, double giaCoBan) {
+    public Sach() {}
+
+    public Sach(String maSach, String tieuDe, String tacGia, int namXuatBan, int soLuong, double giaCoBan) {
+        this.maSach = maSach;
         this.tieuDe = tieuDe;
         this.tacGia = tacGia;
         this.namXuatBan = namXuatBan;
@@ -13,20 +17,70 @@ public abstract class Sach {
         this.giaCoBan = giaCoBan;
     }
 
-    public abstract double tinhGiaBan();
+    public abstract double tinhGiaBan(); 
 
-    public String getTieuDe() { return tieuDe; }
-    public void setTieuDe(String tieuDe) { this.tieuDe = tieuDe; }
+    public String getMaSach() {
+        return maSach;
+    }
 
-    public String getTacGia() { return tacGia; }
-    public void setTacGia(String tacGia) { this.tacGia = tacGia; }
+    public void setMaSach(String maSach) {
+        this.maSach = maSach;
+    }
 
-    public int getNamXuatBan() { return namXuatBan; }
-    public void setNamXuatBan(int namXuatBan) { this.namXuatBan = namXuatBan; }
+    public String getTieuDe() {
+        return tieuDe;
+    }
 
-    public int getSoLuong() { return soLuong; }
-    public void setSoLuong(int soLuong) { this.soLuong = soLuong; }
+    public void setTieuDe(String tieuDe) {
+        this.tieuDe = tieuDe;
+    }
 
-    public double getGiaCoBan() { return giaCoBan; }
-    public void setGiaCoBan(double giaCoBan) { this.giaCoBan = giaCoBan; }
+    public String getTacGia() {
+        return tacGia;
+    }
+
+    public void setTacGia(String tacGia) {
+        this.tacGia = tacGia;
+    }
+
+    public int getNamXuatBan() {
+        return namXuatBan;
+    }
+
+    public void setNamXuatBan(int namXuatBan) {
+        this.namXuatBan = namXuatBan;
+    }
+
+    public int getSoLuong() {
+        return soLuong;
+    }
+
+    public void setSoLuong(int soLuong) {
+        this.soLuong = soLuong;
+    }
+    
+    public double getGiaCoBan() {
+        return giaCoBan;
+    }
+
+    public void setGiaCoBan(double giaCoBan) {
+        this.giaCoBan = giaCoBan;
+    }
+
+    @Override
+    public String toString() {
+        return "Mã sách: " + maSach +
+               "\nTiêu đề: " + tieuDe +
+               "\nTác giả: " + tacGia +
+               "\nNăm xuất bản: " + namXuatBan +
+               "\nSố lượng: " + soLuong +
+               "\nGiá cơ bản: " + String.format("%,.0f VNĐ", giaCoBan);
+    }
+    
+    public void hienThiThongTin() {
+        System.out.println("---------------------------------");
+        System.out.println("Thông tin chi tiết về cuốn sách:");
+        System.out.println(this.toString());
+        System.out.println("---------------------------------");
+    }
 }
