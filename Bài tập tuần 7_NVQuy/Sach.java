301dc497cf9e1e23f7ce542fab65972622a28c61
NgoVoQuy1810 <ngovoquy2006@gmail.com>
2025-10-17 10:36:50 +0700

    Bài tập tuần 7

---
+++ Bài tập tuần 7_NVQuy/Sach.java
@@ -0,0 +1,96 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package NVQ_BTHTuan7;
+
+/**
+ *
+ * @author ADMIN
+ */
+public abstract class Sach {
+    private String maSach;
+    private String tieuDe;
+    private String tacGia;
+    private int namXuatBan;
+    private int soLuong;
+    private double giaCoBan;
+
+    public Sach() {}
+
+    public Sach(String maSach, String tieuDe, String tacGia, int namXuatBan, int soLuong, double giaCoBan) {
+        this.maSach = maSach;
+        this.tieuDe = tieuDe;
+        this.tacGia = tacGia;
+        this.namXuatBan = namXuatBan;
+        this.soLuong = soLuong;
+        this.giaCoBan = giaCoBan;
+    }
+
+    public abstract double tinhGiaBan(); 
+
+    public String getMaSach() {
+        return maSach;
+    }
+
+    public void setMaSach(String maSach) {
+        this.maSach = maSach;
+    }
+
+    public String getTieuDe() {
+        return tieuDe;
+    }
+
+    public void setTieuDe(String tieuDe) {
+        this.tieuDe = tieuDe;
+    }
+
+    public String getTacGia() {
+        return tacGia;
+    }
+
+    public void setTacGia(String tacGia) {
+        this.tacGia = tacGia;
+    }
+
+    public int getNamXuatBan() {
+        return namXuatBan;
+    }
+
+    public void setNamXuatBan(int namXuatBan) {
+        this.namXuatBan = namXuatBan;
+    }
+
+    public int getSoLuong() {
+        return soLuong;
+    }
+
+    public void setSoLuong(int soLuong) {
+        this.soLuong = soLuong;
+    }
+    
+    public double getGiaCoBan() {
+        return giaCoBan;
+    }
+
+    public void setGiaCoBan(double giaCoBan) {
+        this.giaCoBan = giaCoBan;
+    }
+
+    @Override
+    public String toString() {
+        return "Mã sách: " + maSach +
+               "\nTiêu đề: " + tieuDe +
+               "\nTác giả: " + tacGia +
+               "\nNăm xuất bản: " + namXuatBan +
+               "\nSố lượng: " + soLuong +
+               "\nGiá cơ bản: " + String.format("%,.0f VNĐ", giaCoBan);
+    }
+    
+    public void hienThiThongTin() {
+        System.out.println("---------------------------------");
+        System.out.println("Thông tin chi tiết về cuốn sách:");
+        System.out.println(this.toString());
+        System.out.println("---------------------------------");
+    }
+}
