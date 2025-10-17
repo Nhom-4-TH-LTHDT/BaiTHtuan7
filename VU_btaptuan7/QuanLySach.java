83b1f15a7d07bbf7fbc8edece9f8f2edf4674ebf
huynhcongvu <congvuhuynh9@gmail.com>
2025-10-17 18:04:14 +0700

    Update QuanLySach.java

--- VU_btaptuan7/QuanLySach.java
+++ VU_btaptuan7/QuanLySach.java
@@ -1,13 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package NVQ_BTHTuan7;
-
-/**
- *
- * @author ADMIN
- */
+package VU_btaptuan7;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Iterator;
