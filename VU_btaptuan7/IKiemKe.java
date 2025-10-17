0b3d7ac5d1098ca077e742d523a543d4c339b2a7
huynhcongvu <congvuhuynh9@gmail.com>
2025-10-17 18:04:40 +0700

    Refactor IKiemKe interface and clean up comments
    Removed license comments and updated package name.

--- VU_btaptuan7/IKiemKe.java
+++ VU_btaptuan7/IKiemKe.java
@@ -1,24 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
-package NVQ_BTHTuan7;
-
-/**
- *
- * @author ADMIN
- */
+package VU_btaptuan7;
 public interface IKiemKe {
-    /**
-     * Kiểm tra sách có đủ số lượng tối thiểu hay không.
-     * @param soLuongToiThieu Số lượng tối thiểu cần có.
-     * @return true nếu số lượng hiện tại >= số lượng tối thiểu, ngược lại false.
-     */
+
     boolean kiemTraTonKho(int soLuongToiThieu);
 
-    /**
-     * Cập nhật vị trí lưu trữ mới của cuốn sách.
-     * @param viTriMoi Vị trí mới cần cập nhật.
-     */
     void capNhatViTri(String viTriMoi);
 }
